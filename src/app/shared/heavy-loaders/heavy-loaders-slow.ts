d216160b5838cb49867446e4c2c84f5a2cea63f3
NachoGM27 <nachog272@gmail.com>
2025-07-07 15:03:28 +0200

    defer blocks

--- src/app/shared/heavy-loaders/heavy-loaders-slow.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
-  imports: [],
-  template: `<h1>Hola mundo</h1>`
+  imports: [CommonModule],
+  template: `<section [ngClass]="['w-full h-[600px]', cssClass()]">Hola mundo</section>`
 })
 export class HeavyLoadersSlow {
+
+  public cssClass = input.required<string>();
+
+  constructor(){
+    const start = Date.now();
+    //! Codigo bloqueante, NO USAR!!!!
+    while(Date.now() - start < 3000){
+
+    }
+  }
 
 }
