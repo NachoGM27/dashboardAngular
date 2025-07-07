3cfed564289f96c2d0f72e78f2c188f2ad73d9e1
NachoGM27 <nachog272@gmail.com>
2025-07-07 14:44:16 +0200

    changeDetection

--- src/app/dashboard/pages/change-detection/change-detection.ts
+++ src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { Title } from '../../../shared/title/title';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
-  templateUrl: './change-detection.html'
+  imports: [Title, JsonPipe],
+  template: `
+    <app-title [title]="currentFramework()" />
+    <pre>{{frameworkAsSignal() | json}}</pre>
+    <pre>{{frameworkAsproperty | json}}</pre>
+  `
 })
 export default class ChangeDetection {
+
+  public currentFramework = computed(() => `Change detection - ${this.frameworkAsSignal().name}`)
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+  public frameworkAsproperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      //! Si se actualiza una señal se actualiza la property normal, no hacerlo asi
+      //this.frameworkAsproperty.name = 'React';
+      this.frameworkAsSignal.update(value => ({...value, name: 'React'}));
+
+      console.log('Hecho');
+    },3000);
+  }
 
 }
